ff56413608851e260f836b9948ced52eefc05655
Dani-8 <daniyalw6190@gmail.com>
2026-06-15 23:17:54 +0500

    Update

--- frontend/src/components/ui/LoadingSpinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="text-center py-24">
-      <div className="inline-block animate-spin rounded-full h-10 w-10 border-4 border-rose-500 border-t-transparent"></div>
-    </div>
-  );
-}
